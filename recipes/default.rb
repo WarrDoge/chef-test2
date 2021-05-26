305649770d9d4d92dfab3f097121d6a670702dfb
WarrDoge <warr.doge@gmail.com>
2021-05-26 12:45:31 +0300

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 #
 # Cookbook:: chef-server-test
 # Recipe:: default
-#
+package 'chefServer' do
+	package_name 'chef-server-core'
+	default_release 'stable'
+	version 'latests'
+	action :install
+end	
 # Copyright:: 2021, The Authors, All Rights Reserved.
