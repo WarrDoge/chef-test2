2cb58d004248ab51d5224a8c44c8aadfb62a7492
WarrDoge <warr.doge@gmail.com>
2021-05-26 19:39:46 +0300

    New Hope 1.4

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,50 +1,47 @@
-#
 # Cookbook:: chef-server-test
 # Recipe:: default
 
 
-# -----------
-# From source
-# -----------
-
-# Create a package directory
-directory "/chef" do
-	action :create	
-end
-
-
-# Download package
-remote_file '/chef/chef-server-core-14.4.4-1.el7.x86_64.rpm' do
-	source 'https://packages.chef.io/files/stable/chef-server/14.4.4/el/7/chef-server-core-14.4.4-1.el7.x86_64.rpm'
-	action :create_if_missing	
-end
-
-
+# ---------------
 # Install package
+# ---------------
+packageSource = node['packageSource']
+packageName = node['packageName']
 rpm_package 'chefServerCoreInstall' do
-	package_name '/chef/chef-server-core-14.4.4-1.el7.x86_64.rpm'
-	action :install
-end	
+  source packageSource
+  package_name packageName
+  action :install
+end 
 
 
+# -----------------------------------
 # Chef reconfigure and accept license
+# -----------------------------------
 execute 'chefReconfigure' do
-	command 'chef-server-ctl reconfigure --chef-license accept'
+  command 'chef-server-ctl reconfigure --chef-license accept'
+  not_if 'ls /etc/opscode | grep -q pivotal'
 end
 
 
+# -----------
 # Create User
+# -----------
+user = node['userName']
+userMail = node['userMail']
+userPass = (0...10).map { ('a'..'z').to_a[rand(10)] }.join
+File.write("pass.txt", "user: #{user}\nemail: #{userMail}\npassword: #{userPass}\n", mode: "a")
 execute 'chefCreateAdmin' do
-	command 'chef-server-ctl user-create admin admin admin my@email.com root228' #-f /etc/chef/admin.pem
-	not_if 'chef-server-ctl user-list | grep -q admin'
+  command "chef-server-ctl user-create #{user} #{user} #{user} #{userMail} #{userPass}"
+  not_if "chef-server-ctl user-list | grep -q #{user}"
 end
 
 
+# ---------------------------
 # Create org and attache user
+# ---------------------------
+orgName = node['orgName']
+orgFullName = node['orgFullName']
 execute 'chefCreateOrg' do
-	command 'chef-server-ctl org-create someorg "voprosovNet" -a admin'
-	not_if 'chef-server-ctl org-list | grep -q someorg'
+  command "chef-server-ctl org-create #{orgName} ""#{orgFullName}"" -a #{user}"
+  not_if "chef-server-ctl org-list | grep -q #{orgName}"
 end
-
-
-# Copyright:: 2021, The Authors, All Rights Reserved.
