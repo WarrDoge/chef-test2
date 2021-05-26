eed1cfb3561746595b001e5bd7525a970800d817
WarrDoge <warr.doge@gmail.com>
2021-05-26 16:04:44 +0300

    Final 1.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # chef-server-test
 
-TODO: Enter the cookbook description here.
-
+Centos 7, install & configure chef-server-core-14.4.4, create user 'admin' and 'someorg' organization.
+Vagrant memory is set to 4096.
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,35 +7,43 @@
 # From source
 # -----------
 
+# Create a package directory
 directory "/chef" do
 	action :create	
 end
 
 
+# Download package
 remote_file '/chef/chef-server-core-14.4.4-1.el7.x86_64.rpm' do
 	source 'https://packages.chef.io/files/stable/chef-server/14.4.4/el/7/chef-server-core-14.4.4-1.el7.x86_64.rpm'
 	action :create_if_missing	
 end
 
 
+# Install package
 rpm_package 'chefServerCoreInstall' do
 	package_name '/chef/chef-server-core-14.4.4-1.el7.x86_64.rpm'
 	action :install
 end	
 
 
+# Chef reconfigure and accept license
 execute 'chefReconfigure' do
 	command 'chef-server-ctl reconfigure --chef-license accept'
 end
 
 
+# Create User
 execute 'chefCreateAdmin' do
-	command 'chef-server-ctl user-create admin admin admin my@email.com root' #-f /etc/chef/admin.pem'
+	command 'chef-server-ctl user-create admin admin admin my@email.com root228' #-f /etc/chef/admin.pem
+	not_if 'chef-server-ctl user-list | grep -q admin'
 end
 
 
+# Create org and attache user
 execute 'chefCreateOrg' do
-	command 'chef-server-ctl org-create newOrg -a admin'
+	command 'chef-server-ctl org-create someorg "voprosovNet" -a admin'
+	not_if 'chef-server-ctl org-list | grep -q someorg'
 end
 
 
